1ada25016537478d52e2abdb7f94108b58a81aee
Wonhyuk Choi <wc012013@mymail.pomona.edu>
2020-08-25 11:11:45 +0900

    add bfs

--- graphs/bfs.cpp
+++ graphs/bfs.cpp
@@ -1,14 +1,39 @@
 #include <iostream>
 #include <queue>
+#include <unordered_set>
 #include "graph.h"
 
 template <typename TKey, typename TWeight>
-void breadthFirst(Graph<TKey, TWeight>* graph){
-    std::cout << "Hello World" << std::endl;
+void breadthFirst(Graph<TKey, TWeight> &graph){
+    std::unordered_set<TKey> visited;
+    std::queue<TKey> toVisit;
+
+    for(auto &it: graph.vertices){
+        TKey vertexKey = it.second.value;
+        toVisit.push(vertexKey);
+        bool alreadyVisited;
+
+        while(!toVisit.empty()){
+            vertexKey = toVisit.front();
+            toVisit.pop();
+
+            alreadyVisited = visited.find(vertexKey) == visited.end();
+            if(!alreadyVisited)
+                continue;
+
+            visited.insert(vertexKey);
+            std::cout << "Visting vertex " << vertexKey << std::endl;
+
+            auto adjList = graph.vertices.find(vertexKey)->second.adjVertexList;
+            for(auto &adjacent: adjList)
+                toVisit.push(adjacent.key);
+        }
+    }
 }
 
 int main(int argc, char** argv){
-    breadthFirst(&exampleGraph);
+    Graph<int,int> exampleGraph = makeExample();
+    breadthFirst(exampleGraph);
 
     return 0;
 }
