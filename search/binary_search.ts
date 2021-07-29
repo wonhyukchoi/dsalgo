598d5875ac2d1180785fcae7ff5755786252f6d9
Wonhyuk Choi <wc012013@mymail.pomona.edu>
2021-07-28 22:25:25 -0400

    added binary search

--- search/binary_search.ts
+++ search/binary_search.ts
@@ -1,34 +1,19 @@
-class Test {
-    constructor(){}
+// Returns index of number to be searched; if DNE, returns -1.
+// Easily testable using many test cases available at https://leetcode.com/problems/binary-search/
+function binarySearch(nums: number[], target: number): number {
+    return binarySearchHelper(nums, 0, nums.length-1, target);
+};
 
-    assertEqual(x: number, y:number) {
-        if (x != y)
-            throw new Error("Algorithm failed.")
-    }
-
-    testDriver(numArr: number[], nonexistentValues: number[]): void {
-        for (const toSearch of numArr) {
-            const result: number = binarySearch(numArr, toSearch);
-            const expected = toSearch;
-            this.assertEqual(result, expected);
-        }
-        const cannotFind: number = -1;
-        for (const dneValue of nonexistentValues) {
-            const result: number = binarySearch(numArr, dneValue);
-            this.assertEqual(result, cannotFind);
-        }
-    }
-
-    runTests() {
-        this.testDriver([1,2,3,4,5], []);
-        this.testDriver([1,2,3,4,5], [-1,6,10,77,-5]);
-        this.testDriver([0,2,4,6,8,11,15,19,21], [1,3,5,7,12]);
-    }
-}
-
-function binarySearch(arr: number[], val: number): number {
-    return val;
-}
-
-const test: Test = new Test()
-test.runTests()
+function binarySearchHelper(nums: number[], start: number, end: number, target:number): number {
+    if (end < start)
+        return -1;
+        
+    const midIndex = Math.floor((start + end)/2);
+    const midElement = nums[midIndex];
+    if (midElement == target)
+        return midIndex;
+    else if (midElement < target)
+        return binarySearchHelper(nums, midIndex+1, end, target);
+    else // target < midElement
+        return binarySearchHelper(nums, start, midIndex-1, target);
+};
---
+++ sorting/rust_sorts/src/main.rs
@@ -0,0 +1,5 @@
+fn main() {
+    i32 foo = 2;
+    main(foo * 2);
+    a-21414
+}
